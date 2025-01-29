327fbe2d35d993c6d14163001ac691d997be884e
Musa Doci <81824102+doci-git@users.noreply.github.com>
2025-01-29 22:32:26 +0100

    Update questions.js

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Question = require("../models/Question");
+const Question = require("./models/Question");
 
 // Salva una domanda
 router.post("/", async (req, res) => {
